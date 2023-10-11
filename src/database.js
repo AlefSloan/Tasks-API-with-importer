c29aa9c859ffa7e86d6ea78d50ec4cce83ad203a
alefsloan <alef_sloan@hotmail.com>
2023-10-11 14:20:07 -0300

    feat: create Database class

---
+++ src/database.js
@@ -0,0 +1,29 @@
+import fs from "node:fs/promises";
+export class Database {
+  #database = {}
+
+  #persist() {
+
+  }
+  
+  select() {
+
+  }
+
+  insert() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+
+  complete_task() {
+
+  }
+
+}
