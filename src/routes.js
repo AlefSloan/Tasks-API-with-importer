a7974e5a9ec95c2ca4f22f388f407c79a0987b5a
alefsloan <alef_sloan@hotmail.com>
2023-10-11 14:16:27 -0300

    feat: connect server to routes

--- src/routes.js
+++ src/routes.js
@@ -3,35 +3,35 @@ export const routes = [
     method: 'GET',
     path: '/tasks',
     handler: (req, res) => {
-      res.end();
+      res.end('This is a GET Route');
     },
   },
   {
     method: 'POST',
     path: '/tasks',
     handler: (req, res) => {
-      res.end();
+      res.end('This is a POST Route');
     },
   },
   {
     method: 'PUT',
     path: '/tasks/:id',
     handler: (req, res) => {
-      res.end();
+      res.end('This is a PUT Route');
     },
   },
   {
     method: 'PATCH',
     path: '/tasks/:id',
     handler: (req, res) => {
-      res.end();
+      res.end('This is a PATCH Route');
     },
   },
   {
     method: 'DELETE',
     path: '/tasks/:id',
     handler: (req, res) => {
-      res.end();
+      res.end('This is a DELETE Route');
     },
   },
 ];
