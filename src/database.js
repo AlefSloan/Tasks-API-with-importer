b360ea972942da0cec83aa0052ef2d2094d6b7e7
alefsloan <alef_sloan@hotmail.com>
2023-10-11 14:57:09 -0300

    create write and read in memory database

--- src/database.js
+++ src/database.js
@@ -1,13 +1,25 @@
 import fs from "node:fs/promises";
+
+const databasePath = new URL('db/db.json', import.meta.url);
+
 export class Database {
   #database = {}
 
-  #persist() {
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data);
+      }).catch(() => {
+        this.#persist();
+      });
+  }
 
+  #persist() {
+    return fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
   
   select() {
-
+    
   }
 
   insert() {
